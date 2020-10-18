7c73fba5a2c2989fa17264f17000fb23f6a188ec
Gabriel Acevedo <u631186@T0003540283.local>
2020-10-17 22:12:11 -0300

    final

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -35,7 +35,7 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final sockectService = Provider.of<SocketService>(context, listen: false);
+    final sockectService = Provider.of<SocketService>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
